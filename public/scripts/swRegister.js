2fd1db47ffedbd032c2d6095dd3344f50e8fb4b6
ivanterzic <ivan.terzic@fer.hr>
2023-12-07 20:54:12 +0100

    funkcionalnosti same aplikacije gotove

---
+++ public/scripts/swRegister.js
@@ -0,0 +1,5 @@
+navigator.serviceWorker.register('sw.js').then(function(registration) {
+    console.log('Service worker registered ! ', registration);
+}).catch(function(err) {
+    console.log('Service worker registration failed: ', err);
+});
